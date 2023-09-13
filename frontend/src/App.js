329762610f2c9f5c4b00bc772bde0bcde82a9291
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2023-09-13 19:29:31 +0530

    ratings component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,15 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 import {Outlet} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
    <>
    <Header/>
    <main className='py-3'>
     <Container>
-      <Outlet/>
+    {/* <HomeScreen /> */}
+    <Outlet/>
     </Container>
    </main>
    <Footer/>
